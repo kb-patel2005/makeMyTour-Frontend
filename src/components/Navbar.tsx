cef872f05749f2d1d59a00af2e1c629541f44c2a
kb-patel2005 <krishpatel1181@gmail.com>
2026-04-18 17:02:40 +0530

    let's go first time i commit after making repository3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,14 +50,16 @@ const Navbar = () => {
 
       <div className="max-w-7xl mx-auto px-6 md:px-10 flex items-center h-16">
 
-        <div className="flex items-center space-x-2">
-          <Plane className="w-7 h-7 text-red-500" />
-          <span className="text-xl md:text-2xl font-bold">MakeMyTour</span>
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-center space-x-2">
+            <Plane className="w-7 h-7 text-red-500" />
+            <span className="text-xl md:text-2xl font-bold">MakeMyTour</span>
+          </div>
+        </Link>
 
         <div className="flex-1" />
 
-                <div className="hidden md:flex items-center gap-8">
+        <div className="hidden md:flex items-center gap-8">
           {user ? (
             <>
               <Notification trigger={<Bell className="w-5 h-5 cursor-pointer" />} />
